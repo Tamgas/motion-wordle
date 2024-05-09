c1ab222c6dd3441dc84095e994a2cb89c9341473
seyitkulov <islamsejtkulov@gmail.com>
2024-05-09 16:14:22 +0600

    UI gototvo

---
+++ src/components/Cube/Cube.tsx
@@ -0,0 +1,55 @@
+import "../Cube/Cube.css";
+
+function Cube() {
+    return (
+        <div id="cube">
+            <div className="container">
+                <div className="play">
+                    <div className="play__row">
+                        <div className="play-row-inputs">
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                        </div>
+                        <div className="play-row-inputs">
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                        </div>
+                        <div className="play-row-inputs">
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                        </div>
+                        <div className="play-row-inputs">
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                        </div>
+                        <div className="play-row-inputs">
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                            <input></input>
+                        </div>
+                    </div>
+                    <div className="wordle-text">
+                        <p>Wordle #195</p>
+                        <p>12/31/2021</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cube;
